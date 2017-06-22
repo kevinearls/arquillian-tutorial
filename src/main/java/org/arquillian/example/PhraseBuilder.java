7f6c3d2515e92ff90fadb622b276f1376e156d7b
Kevin Earls <kevin@kevinearls.com>
2017-06-22 15:09:40 +0200

    First refactor, added PhraseBuilder

--- src/main/java/org/arquillian/example/PhraseBuilder.java
+++ src/main/java/org/arquillian/example/PhraseBuilder.java
@@ -1,7 +1,20 @@
 package org.arquillian.example;
 
-/**
- * Created by kearls on 22/06/2017.
- */
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+
 public class PhraseBuilder {
+    private Map<String, String> templates;
+
+    public String buildPhrase(String id, Object... args) {
+        return MessageFormat.format(templates.get(id), args);
+    }
+
+    @PostConstruct
+    public void initialize() {
+        templates = new HashMap<String, String>();
+        templates.put("hello", "Hello, {0}!");
+    }
 }
--- src/test/java/org/arquillian/example/GreeterTest.java
+++ src/test/java/org/arquillian/example/GreeterTest.java
@@ -27,7 +27,7 @@ public class GreeterTest {
     @Deployment
     public static JavaArchive createDeployment() {
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
-                .addClass(Greeter.class)
+                .addClasses(Greeter.class, PhraseBuilder.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
         System.out.println(jar.toString(true));
         return jar;
